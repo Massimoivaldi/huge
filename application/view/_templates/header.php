61ea982a0351302c471b660872987c5248b5c7cb
Massimo Ivaldi <info@marketingmix.tf>
2017-03-19 23:13:10 +0100

    bootstrap css

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -8,6 +8,7 @@
     <link rel="icon" href="data:;base64,=">
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
     <!-- wrapper, to center website -->
