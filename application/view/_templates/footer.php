149365d6ae4d0cb37aca9d4764679a129207202c
Massimo Ivaldi <info@marketingmix.tf>
2017-03-19 23:14:34 +0100

    Update footer.php

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -3,5 +3,6 @@
 
     <!-- the support button on the top right -->
     <a class="support-button" href="https://affiliates.a2hosting.com/idevaffiliate.php?id=4471&url=579" target="_blank"></a>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
